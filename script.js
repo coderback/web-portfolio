467d92f0d3e1019375683d9a7f9bb361246fe8c0
Oluwatobi Ojebiyi <tobiojebiyi@gmail.com>
2025-02-16 23:52:17 +0000

    feat(portfolio): base web page finished

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// JavaScript Document
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
