b2dc94f31ce357efb95c7f4c89892419b833e001
Oluwatobi Ojebiyi <tobiojebiyi@gmail.com>
2025-02-25 19:42:55 +0000

    feat(hamburger): updated hamburger button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
+  const icon = document.querySelector(".menu-btn");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
