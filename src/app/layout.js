8f3d85a82722619d0c37c7ce1f23b0cd57558c1f
Timothy Oluwatobi Ojebiyi <tobiojebiyi@gmail.com>
2025-05-22 03:40:09 +0100

    fix(layout): proper implementation of Analytics and Speed Insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      <SpeedInsights />
+      </body>
     </html>
   );
 }
